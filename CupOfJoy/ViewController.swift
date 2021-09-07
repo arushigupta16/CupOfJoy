d86abaaed376fe3e55bdaf91602d1a09b3b1eace
agupta674 <arushigupta@gatech.edu>
2021-09-06 20:20:33 -0400

    Initial Commit

---
+++ CupOfJoy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CupOfJoy
+//
+//  Created by Arushi Gupta on 9/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
